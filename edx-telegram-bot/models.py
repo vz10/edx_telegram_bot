f97db9d703ac9ca5c594668aabf1906675731f70
Dmitry Shyshov <xahgmah@yandex.ru>
2016-02-18 19:18:52 +0000

    udpate model EdxTelegramUser

--- edx-telegram-bot/models.py
+++ edx-telegram-bot/models.py
@@ -1,8 +1,14 @@
+import hashlib
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class EdxTelegramUser(models.Model):
+    """
+    Relations between edx telegram users
+    """
     STATUS_NEW = 'new'
     STATUS_ACTIVE = 'active'
     STATUS_DONE = 'done'
@@ -12,8 +18,28 @@ class EdxTelegramUser(models.Model):
         (STATUS_DONE, 'Done')
     )
     student = models.ForeignKey(User, db_index=True)
-    hash = models.CharField(max_length=64)
-    telegram_id = models.CharField(max_length=10)
+    hash = models.CharField(max_length=36, blank=True, null=True)
+    telegram_id = models.CharField(max_length=10, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True, db_index=True)
     modified = models.DateTimeField(auto_now=True, db_index=True)
     status = models.CharField(max_length=6, choices=STATUSES, default=STATUS_NEW)
+
+    def generate_hash(self):
+        """
+        generate key for authorization
+        :return: string
+        """
+        str_to_hash = str(self.student.pk) + str(self.student.username) + str(self.modified)
+
+        return "hash" + hashlib.md5(str_to_hash).hexdigest()
+
+    def post_save(self, sender, instance, created, **kwargs):
+        """
+        Method for post_save signal which creates a auth hash
+        """
+        if created and not instance.hash_key:
+            instance.hash_key = self.generate_hash()
+            instance.save()
+
+
+post_save.connect(EdxTelegramUser.post_save, EdxTelegramUser, dispatch_uid='add_hash')
