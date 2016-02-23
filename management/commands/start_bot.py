0802960291dea29cc5fe0ac7ceeedf0813716e65
xahgmah <xahgmah@yandex.ru>
2016-02-23 12:21:05 +0200

    clean, refactor app

--- management/commands/start_bot.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.core.management.base import NoArgsCommand
-from edx_telegram_bot.edx_telegram_bot.edx_telegram_bot import RaccoonBot
-
-
-class Command(NoArgsCommand):
-    def handle(self, **options):
-        RaccoonBot()
