6a9b03e54986cc54dc4b5211791cc69757bc531b
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2016-02-25 21:37:10 +0200

    Fixing git strange behaviour

--- edx_telegram_bot/admin.py
+++ edx_telegram_bot/admin.py
@@ -3,15 +3,25 @@ django admin pages for edx-telegram bot model
 '''
 
 from models import (EdxTelegramUser, TfidMatrixAllCourses, MatrixEdxCoursesId,
-                    TfidUserVector, LearningPredictionForUser, PredictionForUser)
+                 TfidUserVector, LearningPredictionForUser, PredictionForUser)
 from ratelimitbackend import admin
 
-admin.site.register(EdxTelegramUser)
+
+class EdxTelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('student', 'status', 'telegram_id', 'created', 'modified')
+    list_filter = ('student', 'status')
+    readonly_fields = ('hash',)
+
 admin.site.register(TfidMatrixAllCourses)
 admin.site.register(MatrixEdxCoursesId)
 admin.site.register(TfidUserVector)
-admin.site.register(LearningPredictionForUser)
 admin.site.register(PredictionForUser)
 
+class LearningPredictionForUserAdmin(admin.ModelAdmin):
+    list_display = ('telegram_user', 'prediction_list')
+    list_filter = ('telegram_user', 'prediction_list')
 
+
+admin.site.register(EdxTelegramUser, EdxTelegramUserAdmin)
+admin.site.register(LearningPredictionForUser, LearningPredictionForUserAdmin)
 
