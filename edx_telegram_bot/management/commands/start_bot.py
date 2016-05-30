8d82c5aff3b1f8c5230cfef796f93cfff8c1712d
Ilya Batozskiy <vZ@vZs-iMac-3.local>
2016-05-30 21:28:16 +0300

    Predeploy improvements

--- edx_telegram_bot/management/commands/start_bot.py
+++ edx_telegram_bot/management/commands/start_bot.py
@@ -4,16 +4,18 @@ from django.conf import settings
 
 from ...edx_telegram_bot import RaccoonBot
 from ...edx_course_bot import CourseBot
-from ...edx_course_xblock_bot import CourseBot
+from ...edx_course_xblock_bot import CourseBot as XblockCourseBot
 
 from ...models import TelegramBot
 
 
 class Command(NoArgsCommand):
     def handle(self, **options):
-        # RaccoonBot()
+        RaccoonBot()
+        CourseBot(token=187925341:AAFTaWDWkTDjFPOJLlA1ArRiK-Sviypx8QM,
+        	collection='111/111/111')
         # map(lambda each: CourseBot(token=each.bot.token,
         #                            collection=each.course_key),
         #     BotFriendlyCourses.objects.all())
-        map(lambda each: CourseBot(token=each.token), TelegramBot.objects.all())
+        map(lambda each: XblockCourseBot(token=each.token), TelegramBot.objects.all())
 
--- edx_telegram_bot/templates/cms/add_bot_button.js
+++ edx_telegram_bot/templates/cms/add_bot_button.js
@@ -17,8 +17,8 @@ function check_course(method){
     var url = "/api/bot/bot_course/";
     var data = window.location.pathname;
  	$.ajax({
-        url: 'http://127.0.0.1:8000' + url,
-     // url: '${Site.objects.get_current()}' + url,
+        // url: 'http://127.0.0.1:8000' + url,
+        url: '${Site.objects.get_current()}' + url,
         data: {key: data},
         type: method,
         csrf: '${ csrf_token }',
